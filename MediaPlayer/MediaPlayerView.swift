d714f26b3b09bc6f57f4b6245dad39df0e22bd72
ms2138 <m38371@gmail.com>
2021-09-27 21:48:09 -0700

    Add activityIndicatorView, mediaView, and mediaPlayer properties.  Add init and setup methods to MediaPlayerView

--- MediaPlayer/MediaPlayerView.swift
+++ MediaPlayer/MediaPlayerView.swift
@@ -8,5 +8,42 @@
 import UIKit
 
 class MediaPlayerView: UIView {
+    let activityIndicatorView = UIActivityIndicatorView(style: .medium)
+    private let mediaView = UIView(frame: .zero)
+    private let mediaPlayer = VLCMediaPlayer()
 
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        setup()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+
+        setup()
+    }
+
+    private func setup() {
+        activityIndicatorView.isUserInteractionEnabled = false
+        activityIndicatorView.translatesAutoresizingMaskIntoConstraints = false
+        activityIndicatorView.color = .white
+
+        mediaView.isUserInteractionEnabled = false
+        mediaView.translatesAutoresizingMaskIntoConstraints = false
+
+        addSubview(mediaView)
+        addSubview(activityIndicatorView)
+
+        NSLayoutConstraint.activate([
+            mediaView.topAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor),
+            mediaView.leadingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.leadingAnchor),
+            mediaView.trailingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.trailingAnchor),
+            mediaView.bottomAnchor.constraint(equalTo: self.safeAreaLayoutGuide.bottomAnchor),
+            activityIndicatorView.centerYAnchor.constraint(equalTo: self.safeAreaLayoutGuide.centerYAnchor),
+            activityIndicatorView.centerXAnchor.constraint(equalTo: self.safeAreaLayoutGuide.centerXAnchor),
+        ])
+
+        mediaPlayer.drawable = mediaView
+    }
 }
