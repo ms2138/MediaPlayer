4d307fc236bad89a66cba39138af66100b84fa25
ms2138 <m38371@gmail.com>
2021-09-28 23:12:36 -0700

    Add playButton and isPlaying properties.  Add view setup for mainView, topView, bottomView, and playButton subviews in MediaControlView

--- MediaPlayer/Views/MediaControlView.swift
+++ MediaPlayer/Views/MediaControlView.swift
@@ -12,6 +12,10 @@ class MediaControlView: UIView {
     var topView = UIView(frame: .zero)
     var bottomView = UIView(frame: .zero)
 
+    var playButton = UIButton(type: UIButton.ButtonType.custom)
+
+    var isPlaying: Bool = false
+
     override public init(frame: CGRect) {
         super.init(frame: frame)
 
@@ -30,6 +34,19 @@ class MediaControlView: UIView {
         mainView.translatesAutoresizingMaskIntoConstraints = false
         topView.translatesAutoresizingMaskIntoConstraints = false
         bottomView.translatesAutoresizingMaskIntoConstraints = false
+
+        playButton.translatesAutoresizingMaskIntoConstraints = false
+
+        addSubview(mainView)
+        mainView.addSubview(topView)
+        mainView.addSubview(bottomView)
+        mainView.backgroundColor = UIColor(white: 0, alpha: 0.0)
+
+        playButton.tag = 0
+        playButton.setImage(isPlaying ? UIImage(named: "pause") : UIImage(named: "play"),  for: .normal)
+        playButton.setImage(isPlaying ? UIImage(named: "play") : UIImage(named: "pause"), for: .highlighted)
+
+        bottomView.addSubview(playButton)
     }
 
     func setupConstraints() {
