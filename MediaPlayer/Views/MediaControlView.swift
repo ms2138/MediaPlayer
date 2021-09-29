43820616315e322f2937d847b87aa376a8095d78
ms2138 <m38371@gmail.com>
2021-09-28 22:12:38 -0700

    Add mainView, topView, and bottomView properties.  Add init and setup methods to MediaControlView

--- MediaPlayer/Views/MediaControlView.swift
+++ MediaPlayer/Views/MediaControlView.swift
@@ -8,5 +8,31 @@
 import UIKit
 
 class MediaControlView: UIView {
+    var mainView = UIView(frame: .zero)
+    var topView = UIView(frame: .zero)
+    var bottomView = UIView(frame: .zero)
 
+    override public init(frame: CGRect) {
+        super.init(frame: frame)
+
+        setupUI()
+        setupConstraints()
+    }
+
+    required public init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+
+        setupUI()
+        setupConstraints()
+    }
+
+    func setupUI() {
+        mainView.translatesAutoresizingMaskIntoConstraints = false
+        topView.translatesAutoresizingMaskIntoConstraints = false
+        bottomView.translatesAutoresizingMaskIntoConstraints = false
+    }
+
+    func setupConstraints() {
+
+    }
 }
