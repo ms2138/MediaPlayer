36ea2f9ec4e95908c795298f431eb817f0558e4d
ms2138 <m38371@gmail.com>
2021-10-26 21:40:54 -0700

    Add url, credential, and client properties.  Add init method to initialize SMB2Client's properties

--- MediaPlayer/SMB2Client.swift
+++ MediaPlayer/SMB2Client.swift
@@ -9,5 +9,16 @@ import Foundation
 import AMSMB2
 
 class SMB2Client {
+    let url: URL
+    let credential: URLCredential
 
+    private var client: AMSMB2
+
+    init?(url: URL, credential: URLCredential) {
+        self.url = url
+        self.credential = credential
+        guard let client = AMSMB2(url: url, credential: credential) else { return nil }
+
+        self.client = client
+    }
 }
