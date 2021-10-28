db2b27ff86566b53b7479318da32c215a070dc12
ms2138 <m38371@gmail.com>
2021-10-27 22:18:02 -0700

    Add AuthenticationViewController class

---
+++ MediaPlayer/Controllers/AuthenticationViewController.swift
@@ -0,0 +1,27 @@
+//
+//  AuthenticationViewController.swift
+//  MediaPlayer
+//
+//  Created by mani on 2021-10-27.
+//
+
+import UIKit
+
+class AuthenticationViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
+extension AuthenticationViewController {
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+}
