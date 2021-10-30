d9b5ba00019fa03116e9e40f89f5e8fd30a7cd28
ms2138 <m38371@gmail.com>
2021-10-30 15:06:34 -0700

    Add CustomTabBarController class

---
+++ MediaPlayer/Controllers/CustomTabBarController.swift
@@ -0,0 +1,15 @@
+//
+//  CustomTabBarController.swift
+//  MediaPlayer
+//
+//  Created by mani on 2021-10-30.
+//
+
+import UIKit
+
+class CustomTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
