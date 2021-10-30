d1525943177977baabce4a644877926bfe04151b
ms2138 <m38371@gmail.com>
2021-10-29 23:16:24 -0700

    Add serverCell, usernameCell, and passwordCell properties.  Add table view data spruce methods to return number of sections and rows in AuthenticationViewController

--- MediaPlayer/Controllers/AuthenticationViewController.swift
+++ MediaPlayer/Controllers/AuthenticationViewController.swift
@@ -8,6 +8,9 @@
 import UIKit
 
 class AuthenticationViewController: UITableViewController {
+    var serverCell: TextInputCell!
+    var usernameCell: TextInputCell!
+    var passwordCell: TextInputCell!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,10 +21,10 @@ extension AuthenticationViewController {
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return 3
     }
 }
