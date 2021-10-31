ecfe557a0f761e1c309cd467cad90cbfef6cee85
ms2138 <m38371@gmail.com>
2021-10-30 20:22:07 -0700

    Add reuseIdentifier and shares property.  Add table view data source methods to return a share cell in SharesViewController

--- MediaPlayer/Controllers/SharesViewController.swift
+++ MediaPlayer/Controllers/SharesViewController.swift
@@ -8,31 +8,36 @@
 import UIKit
 
 class SharesViewController: UITableViewController {
+    private let reuseIdentifier = "ShareCell"
+
+    var shares = [Share]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-    }
 
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: reuseIdentifier)
+    }
+}
+
+extension SharesViewController {
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return shares.count
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath)
 
-        // Configure the cell...
+        let share = shares[indexPath.row]
+
+        cell.imageView?.image = UIImage(systemName: "folder")
+        cell.textLabel?.text = share.name
 
         return cell
     }
-    */
-
 }
