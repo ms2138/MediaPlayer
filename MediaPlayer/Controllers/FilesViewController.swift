0257ff722843abee346f5e1c3ab9d03f693a6cbc
ms2138 <m38371@gmail.com>
2021-10-30 20:38:33 -0700

    Add FilesViewController class

---
+++ MediaPlayer/Controllers/FilesViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FilesViewController.swift
+//  MediaPlayer
+//
+//  Created by mani on 2021-10-30.
+//
+
+import UIKit
+
+class FilesViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+}
