93d70571256a4af0df504b82555827bc9839e5c8
ms2138 <m38371@gmail.com>
2021-10-30 20:40:18 -0700

    Add smbClient property and init methods to FilesViewController

--- MediaPlayer/Controllers/FilesViewController.swift
+++ MediaPlayer/Controllers/FilesViewController.swift
@@ -8,6 +8,17 @@
 import UIKit
 
 class FilesViewController: UITableViewController {
+    var smbClient: SMB2Client
+
+    init(style: UITableView.Style, smbClient: SMB2Client) {
+        self.smbClient = smbClient
+
+        super.init(style: style)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
