f06e78fd7dc5ef8c28ecf82c00486c8ac4dd1df7
ms2138 <m38371@gmail.com>
2021-10-31 14:09:39 -0700

    Add MediaPlayerViewController class

---
+++ MediaPlayer/Controllers/MediaPlayerViewController.swift
@@ -0,0 +1,15 @@
+//
+//  MediaPlayerViewController.swift
+//  MediaPlayer
+//
+//  Created by mani on 2021-10-31.
+//
+
+import UIKit
+
+class MediaPlayerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
