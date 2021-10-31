ee8aa95cb45bb2299b3897ba1a0f77ede853f9e6
ms2138 <m38371@gmail.com>
2021-10-30 20:08:12 -0700

    Add SharesViewController class

---
+++ MediaPlayer/Controllers/SharesViewController.swift
@@ -0,0 +1,38 @@
+//
+//  SharesViewController.swift
+//  MediaPlayer
+//
+//  Created by mani on 2021-10-30.
+//
+
+import UIKit
+
+class SharesViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+    /*
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    */
+
+}
