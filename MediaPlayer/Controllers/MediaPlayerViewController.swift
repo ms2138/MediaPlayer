635ddc38def3321aa592aa1c9f66908dde516e18
ms2138 <m38371@gmail.com>
2021-10-31 19:33:03 -0700

    Add mediaPlayer and url properties.  Add init and loadVideoStream methods.  Add mediaPlayer setup code in MediaPlayerViewController's viewDidLoad method

--- MediaPlayer/Controllers/MediaPlayerViewController.swift
+++ MediaPlayer/Controllers/MediaPlayerViewController.swift
@@ -8,8 +8,44 @@
 import UIKit
 
 class MediaPlayerViewController: UIViewController {
+    let mediaPlayer: MediaPlayerView
+    var url: URL {
+        willSet {
+            mediaPlayer.loadVideo(from: newValue)
+        }
+    }
+
+    init(streamURL url: URL) {
+        self.mediaPlayer = MediaPlayerView()
+        self.url = url
+
+        super.init(nibName: nil, bundle: nil)
+
+        mediaPlayer.translatesAutoresizingMaskIntoConstraints = false
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        view.addSubview(mediaPlayer)
+
+        NSLayoutConstraint.activate([
+            mediaPlayer.topAnchor.constraint(equalTo: view.topAnchor),
+            mediaPlayer.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            mediaPlayer.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            mediaPlayer.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+        ])
+
+        navigationController?.setNavigationBarHidden(true, animated: false)
+
+        loadVideoStream(for: url)
+    }
+
+    func loadVideoStream(for url: URL) {
+        mediaPlayer.loadVideo(from: url)
     }
 }
